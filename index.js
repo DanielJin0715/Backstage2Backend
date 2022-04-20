18547dd77bb4ec9171f654ec92979d80eda3e909
Cocoa Dev <babypigdev@gmail.com>
2022-04-20 16:04:35 +0900

    fix cors

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const mailgun = require("mailgun-js")({
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
